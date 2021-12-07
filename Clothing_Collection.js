d14516b238b6175d7f6f545bcc2b1bf3dd6633f0
EmGrave <90418854+EmGrave@users.noreply.github.com>
2021-12-07 15:49:04 -0500

    Add files via upload

---
+++ Clothing_Collection.js
@@ -0,0 +1,79 @@
+$(function() {
+  $("#drag-me").draggable();
+});
+
+$(function() {
+  $("#drag-me1").draggable();
+});
+
+$(function() {
+  $("#drag-me2").draggable();
+});
+
+$(function() {
+  $("#drag-me3").draggable();
+});
+
+$(function() {
+  $("#drag-me4").draggable();
+});
+
+$(function() {
+  $("#drag-me5").draggable();
+});
+
+$(function() {
+  $("#drag-me6").draggable();
+});
+
+$(function() {
+  $("#drag-me7").draggable();
+});
+
+$(function() {
+  $("#drag-me8").draggable();
+});
+
+$(function() {
+  $("#drag-me9").draggable();
+});
+
+$(function() {
+  $("#drag-me10").draggable();
+});
+
+$(function() {
+  $("#drag-me11").draggable();
+});
+
+$(function() {
+  $("#drag-me12").draggable();
+});
+
+$(function() {
+  $("#drag-me13").draggable();
+});
+
+$(function() {
+  $("#drag-me14").draggable();
+});
+
+$(function() {
+  $("#drag-me15").draggable();
+});
+
+$(function() {
+  $("#drag-me16").draggable();
+});
+
+$(function() {
+  $("#drag-me17").draggable();
+});
+
+$(function() {
+  $("#drag-me18").draggable();
+});
+
+$(function() {
+  $("#drag-me19").draggable();
+});
